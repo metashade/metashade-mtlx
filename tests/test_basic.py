9c2c9c4251ae82e6071739482d5d10ffadef28a6
Pavlo Penenko <pavlo.penenko@gmail.com>
2025-11-25 22:17:12 -0500

    Fix `_base` being imported from Metashade mistakenly

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import _base
+from . import _base
 
 class TestBasic:
     def test_dummy_purple(self):
