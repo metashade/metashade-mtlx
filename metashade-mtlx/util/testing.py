8f20da636cb4ea1e37230a12b13b5e92d812fd8e
Pavlo Penenko <pavlo.penenko@gmail.com>
2025-11-29 16:53:07 -0500

    First-ever unit test - replaces the CLI script (#7)
    * First version of first test
    
    * Refactor `GeneratorContext` in preparation for moving its uses to tests
    
    * Simplify `GlslTestContext`
    
    * Clean up tests
    
    * Fix the tests
    
    * Test references
    
    * Enable the superrepo tests in VS Code
    
    * Fix `_base` being imported from Metashade mistakenly
    
    * First ever unit test!
    
    * Remove the non-test version of "generate purple"
    
    * Remove the launch config for the non-test version

---
+++ metashade-mtlx/util/testing.py
@@ -0,0 +1,31 @@
+# Copyright 2025 Pavlo Penenko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import inspect
+import os
+from pathlib import Path
+
+from metashade.util.testing import get_test_func_name
+from generate import GlslGeneratorContext
+
+class GlslTestContext(GlslGeneratorContext):
+    @classmethod
+    def setup_class(cls, test_dir: Path):
+        cls._parent_dir = test_dir
+        cls._out_dir = test_dir.parent / 'libraries'
+        os.makedirs(cls._out_dir, exist_ok=True)
+
+    def __init__(self):
+        test_name = get_test_func_name()
+        super().__init__(test_name, self._out_dir)
---
+++ tests/test_basic.py
@@ -0,0 +1,34 @@
+# Copyright 2025 Pavlo Penenko
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from util.testing import GlslTestContext
+
+class TestBasic:
+    def test_dummy_purple(self):
+        """Generate a simple purple color function."""
+        ctx = GlslTestContext()
+        
+        with ctx as test_ctx:
+            sh = test_ctx._sh
+            func_name = 'dummy_purple'
+            
+            # Define the purple function
+            with sh.function(func_name)(result=sh.Out(sh.RgbF)):
+                sh.result = sh.RgbF((0.5, 0.0, 1.0))
+            
+            # Add as MaterialX node
+            test_ctx.add_node(
+                func_name=func_name,
+                mx_doc_string='Metashade-generated dummy node that returns a purple color'
+            )
